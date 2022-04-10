17a26287696e6413d3da8fc8bd50e5fa0c75f182
Lucas Novaes <lucasmsn200@gmail.com>
2022-04-10 12:01:59 -0300

    chore: adding headless ui and remix icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import './index.css';
 
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </>
+  );
 }
 
 export default App;
